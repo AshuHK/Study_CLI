a0b86b5d781aa03e8155a120d18db4fd34407229
Ashu Kher <ashukher1@gmail.com>
2020-06-22 09:48:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,32 @@
 #include <string>
 #include <vector>
 
+void print_vector(
+    const std::vector<std::pair<std::string, std::string>>& questions) {}
+
 int main() {
   std::cout << "Welcome to Study CLI" << std::endl;
   std::cout << "Your personal command line flashcards :)" << std::endl;
 
-  std::cout << "Enter the file name: "; 
-  std::string file_name;
-  std::getline(std::cin, file_name);
+  while (true) {
+    std::cout << "Enter the file name: ";
+    std::string file_name;
+    std::getline(std::cin, file_name);
 
-  std::ifstream file(file_name);
+    std::ifstream file(file_name);
 
-  if (file.is_open()) {
-    std::cout << "File has opened" << std::endl;
-  } else {
-    std::cout << "The file is not there" << std::endl;
+    if (file_name == "quit" || file_name == "q") {
+      return 0;
+    }
+
+    if (file.is_open()) {
+      break;
+    } else {
+      std::cout << "Try again." << std::endl;
+    }
   }
+
+  std::vector<std::pair<std::string, std::string>> questions;
 
   return 0;
 }
