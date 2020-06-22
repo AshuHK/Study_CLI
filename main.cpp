b9fd0f238b502658f8cc0e91d7c91189113b9eb3
Ashu Kher <ashukher1@gmail.com>
2020-06-22 10:22:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@
 
 void print_vector(
     const std::vector<std::pair<std::string, std::string>>& questions) {
-
-      for(const std::pair<std::string, std::string> & item : questions) {
-        std::cout << std::get<0>(item) << " : " << std::get<1>(item) << std::endl; 
-      }
-    }
+  for (const std::pair<std::string, std::string>& item : questions) {
+    std::cout << std::get<0>(item) << " : " << std::get<1>(item) << std::endl;
+  }
+}
 
 std::vector<std::pair<std::string, std::string>> read_csv(std::ifstream& file) {
   std::vector<std::pair<std::string, std::string>> data;
@@ -43,8 +42,7 @@ int main() {
   }
 
   std::vector<std::pair<std::string, std::string>> questions = read_csv(file);
-
-  print_vector(questions); 
+  print_vector(questions);
 
   return 0;
 }
