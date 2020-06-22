c85ec3a4193168161ebb7a705db77480607fc1f1
Ashu Kher <ashukher1@gmail.com>
2020-06-22 09:16:20 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::cout << "Welcome to Study CLI" << std::endl;
+  std::cout << "Your personal command line flashcards :)" << std::endl;
+  return 0;
+}
