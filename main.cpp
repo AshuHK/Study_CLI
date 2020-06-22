65fcaea4e50dfabcdb18662f15d33504518fc81a
Ashu Kher <ashukher1@gmail.com>
2020-06-22 09:36:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream> 
 
 int main() {
   std::cout << "Welcome to Study CLI" << std::endl;
   std::cout << "Your personal command line flashcards :)" << std::endl;
 
-  std::string file_name; 
-  std::getline(std::cin, file_name); 
+  std::string file_name;
+  std::getline(std::cin, file_name);
 
-  std::ifstream file(file_name); 
+  std::ifstream file(file_name);
 
-  if(file.is_open()) {
-    std::cout << "File has opened" << std::endl; 
+  if (file.is_open()) {
+    std::cout << "File has opened" << std::endl;
   } else {
-    std::cout << "The file is not there" << std::endl; 
+    std::cout << "The file is not there" << std::endl;
   }
 
   return 0;
